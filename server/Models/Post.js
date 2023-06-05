5282f7cfa72e542fac54a2ba0089398bcd3ffa51
Sarthak Agarwal <44892123+Sarthak014@users.noreply.github.com>
2023-06-05 14:56:52 +0530

    feat: updated the script, user modal, routes and added post modal (#11)
    * removed .env from caching
    
    * chore: updated the scripts for running the server
    
    * feat: added routes
    
    * feat: added validation for user name
    
    * chore: added mock data
    
    * feat: rename the route import, and db mock data
    
    * fix upd the user modal def, provide coll name
    
    * feat: define post model

---
+++ server/Models/Post.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+// Schema
+const schemaDefinition = {
+  userId: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  location: String,
+  description: String,
+  picturePath: String,
+  userPicturePath: String,
+  like: {
+    type: Map,
+    of: Boolean,
+  },
+  comments: {
+    type: Array,
+    default: [],
+  },
+};
+
+const postSchema = new mongoose.Schema(schemaDefinition, {
+  collection: "Post",
+  timestamps: {
+    createdAt: true,
+    updatedAt: true,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
