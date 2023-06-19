5feca3c3143a35d82208bb6ac2757cfbdd5c533f
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-06-19 12:00:11 +0530

    feat: defined store and initial state for auth

---
+++ client/src/store/authSlicer.js
@@ -0,0 +1,54 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { initialState } from "./states/initialState";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.usr.friends = action.payload.friends;
+      } else {
+        console.error("User friends does not exists");
+      }
+    },
+
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+
+    updatePost: (state, action) => {
+      const updatedPost = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+
+      state.posts = updatedPost;
+    },
+  },
+});
+
+export const {
+  setMode,
+  setLogin,
+  setLogout,
+  setFriends,
+  setPosts,
+  updatePost,
+} = authSlice.actions;
+
+export default authSlice.reducer;
---
+++ client/src/store/states/initialState.js
@@ -0,0 +1,6 @@
+export const initialState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+};
