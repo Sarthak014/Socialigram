838c6c94002f49200c28eb745c1b6d83ac597286
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-06-19 11:58:02 +0530

    feat: define routes and added in root node

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import router from "router/route";
 
 function App() {
   return (
     <div className="app">
+      <RouterProvider router={router} />
     </div>
   );
 }
