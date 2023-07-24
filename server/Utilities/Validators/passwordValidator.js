70202617fc88a9f3f5a87c2ac392a6b7da55f7a0
Sarthak Agarwal <44892123+Sarthak014@users.noreply.github.com>
2023-07-24 17:57:46 +0530

    fix: regex definition issue and variable declaration from var to const (#23) (#24)

--- server/Utilities/Validators/passwordValidator.js
+++ server/Utilities/Validators/passwordValidator.js
@@ -1,5 +1,4 @@
 export const validatePassword = (pswd) => {
-    const re = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]$";
-
+    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return re.test(pswd);
 }
