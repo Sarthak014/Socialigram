7c3885ebed7c391eae2ab0a09fba1eadbfe44534
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-07-29 17:35:35 +0530

    feat: provided theme in root app and added comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import { RouterProvider } from "react-router-dom";
 import router from "router/route";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </div>
   );
 }
