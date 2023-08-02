8462c1681ef5a2d5ab5e79ac37ffd8ae6d1b4712
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-08-02 13:01:41 +0530

    feat: added user profile widget to home screens and post widget

--- client/src/views/home/index.jsx
+++ client/src/views/home/index.jsx
@@ -1,11 +1,36 @@
 import { Box } from "@mui/material";
 import Navbar from "views/navbar";
+import { useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import UserProfile from "views/widgets/UserProfile";
+import CreatePost from "views/widgets/CreatePost";
 
 const Home = () => {
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <>
       <Box className="home-wrapper">
         <Navbar />
+        <Box
+          display={isNonMobileScreen ? "flex" : "block"}
+          padding="2rem 6%"
+          gap="0.5rem"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+            <UserProfile userId={_id} picturePath={picturePath} />
+          </Box>
+          <Box
+            flexBasis={isNonMobileScreen ? "42%" : undefined}
+            mt={isNonMobileScreen ? undefined : "2rem"}
+          >
+            <CreatePost picturePath={picturePath} />
+          </Box>
+          {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+        </Box>
       </Box>
     </>
   );
